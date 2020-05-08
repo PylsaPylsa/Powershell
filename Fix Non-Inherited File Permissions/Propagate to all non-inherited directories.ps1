9d0403aaf38116ade92208f2af0259fb23cb9667
PylsaPylsa <tommybeta@gmail.com>
2020-05-08 11:01:32 +0200

    Restructure

---
+++ Fix Non-Inherited File Permissions/Propagate to all non-inherited directories.ps1
@@ -0,0 +1,34 @@
+﻿cls
+$group = "GG-FS-Afdelingen"
+$domain ='oosterpoort.local'
+$target = "\\?\F:\"
+$countTotal = [int]0
+$countApply = [int]0
+
+$folders = Get-ChildItem -LiteralPath $target -Directory -recurse
+foreach ($folder in $folders) {
+    $acls = Get-Acl -Path $folder.FullName  
+    if ($acls.AreAccessRulesProtected -eq $true) {
+        foreach ($acl in $acls) {
+            Write-Host $("[*] Checking {0}" -f $folder.FullName) -ForegroundColor "Green"
+            $accs = $acl.Access 
+            $apply = $true 
+            foreach ($acc in  $accs){
+                if ($acc.identityreference.tostring().split('\')[1] -eq $group) {
+                    Write-Host "    [*] Already applied" -ForegroundColor "Cyan"
+                    $apply = $false
+                }
+            }
+            $countTotal++
+            if ($apply -eq $true) {
+                $acl_Rule = new-object System.Security.AccessControl.FileSystemAccessRule ("$domain\$group", "Modify",”ContainerInherit,ObjectInherit”,”None”,”Allow”)
+                $acl.SetAccessRule($ACL_Rule)
+                Write-Host $("    [*] Applying") -ForegroundColor "Green"
+                Set-Acl -Path $folder.FullName -AclObject $acl 
+                $countApply++
+            }
+        }
+    }
+}
+
+Write-Host "[*] $countTotal directories found, applied group to $countApply of them." -ForegroundColor "Cyan"
