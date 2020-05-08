c0918d963d93294ca1bfc65389bffff1e7d3f8e4
PylsaPylsa <tommybeta@gmail.com>
2020-05-08 10:58:09 +0200

    Updated formatting and readme

--- BlockTeamsCreation/BlockTeamsCreation.ps1
+++ BlockTeamsCreation/BlockTeamsCreation.ps1
@@ -5,23 +5,20 @@ Connect-AzureAD
 $GroupName = Read-Host -Prompt 'Naam Security Group'
 
 $settingsObjectID = (Get-AzureADDirectorySetting | Where-object -Property Displayname -Value "Group.Unified" -EQ).id
-if(!$settingsObjectID)
-{
+if(!$settingsObjectID){
 	$template = Get-AzureADDirectorySettingTemplate | Where-object {$_.displayname -eq "group.unified"}
-    $settingsCopy = $template.CreateDirectorySetting()
-    New-AzureADDirectorySetting -DirectorySetting $settingsCopy
-    $settingsObjectID = (Get-AzureADDirectorySetting | Where-object -Property Displayname -Value "Group.Unified" -EQ).id
+	$settingsCopy = $template.CreateDirectorySetting()
+	New-AzureADDirectorySetting -DirectorySetting $settingsCopy
+	$settingsObjectID = (Get-AzureADDirectorySetting | Where-object -Property Displayname -Value "Group.Unified" -EQ).id
 }
 
 $settingsCopy = Get-AzureADDirectorySetting -Id $settingsObjectID
 $settingsCopy["EnableGroupCreation"] = $AllowGroupCreation
 
-if($GroupName)
-{
+if($GroupName){
 	$settingsCopy["GroupCreationAllowedGroupId"] = (Get-AzureADGroup -SearchString $GroupName).objectid
-}
- else {
-$settingsCopy["GroupCreationAllowedGroupId"] = $GroupName
+}else{
+	$settingsCopy["GroupCreationAllowedGroupId"] = $GroupName
 }
 Set-AzureADDirectorySetting -Id $settingsObjectID -DirectorySetting $settingsCopy
 
--- BlockTeamsCreation/README.md
+++ BlockTeamsCreation/README.md
@@ -6,3 +6,6 @@ This script will block the creation of Office 365 groups (and thus Teams) for al
 - Global Administrator role on tenant
 - [AzureAD Powershell module](https://docs.microsoft.com/en-us/office365/enterprise/powershell/connect-to-office-365-powershell):
   -  `Install-Module -Name AzureAD`
+  
+## Source
+Taken from [Microsoft documentation](https://docs.microsoft.com/en-us/microsoft-365/admin/create-groups/manage-creation-of-groups?view=o365-worldwide) and made minor adjustments.
