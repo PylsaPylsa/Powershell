7dccca2cbb66e3b2f4ef56e39132725f31a89f43
PylsaPylsa <tommybeta@gmail.com>
2020-05-15 09:06:16 +0200

    Create MachineStatus.ps1

---
+++ Citrix Machine Status PRTG Sensor/MachineStatus.ps1
@@ -0,0 +1,38 @@
+Add-PSSnapin citrix*
+
+$DeliveryController = "AAA-BBB-DDC-01.contoso.local"
+
+$retXml = ""
+$retXml += "<prtg>`n"
+
+$Machines = Get-BrokerMachine -AdminAddress $DeliveryController
+$DeliveryGroups = Get-BrokerDesktopGroup -AdminAddress $DeliveryController
+
+foreach($DeliveryGroup in $DeliveryGroups){
+
+    $MachinesInMaintenance = $Machines | Where-Object {$_.InMaintenanceMode -eq $True -and $_.PowerState -eq "On" -and $_.DesktopGroupName -eq $DeliveryGroup.Name}
+    $MachinesPoweredOff = $Machines | Where-Object {$_.PowerState -eq "Off" -and $_.DesktopGroupName -eq $DeliveryGroup.Name}
+    $MachinesUnregistered = $Machines | Where-Object {$_.RegistrationState -eq "Unregistered" -eq $True -and $_.PowerState -eq "On" -and $_.DesktopGroupName -eq $DeliveryGroup.Name}
+
+
+    $retXml += "  <result>`n"
+    $retXml += "    <channel>[$($DeliveryGroup.Name)] Aantal machines in maintenance</channel>`n"
+    $retXml += "    <value>$($MachinesInMaintenance.Count)</value>`n"
+    $retXml += "  </result>`n"
+
+    $retXml += "  <result>`n"
+    $retXml += "    <channel>[$($DeliveryGroup.Name)] Aantal machines uitgeschakeld</channel>`n"
+    $retXml += "    <value>$($MachinesPoweredOff.Count)</value>`n"
+    $retXml += "  </result>`n"
+
+    $retXml += "  <result>`n"
+    $retXml += "    <channel>[$($DeliveryGroup.Name)] Aantal machines unregistered</channel>`n"
+    $retXml += "    <value>$($MachinesUnregistered.Count)</value>`n"
+    $retXml += "  </result>`n"
+
+}
+
+$retXml += "</prtg>"
+
+write-host $retXml
+exit 0
