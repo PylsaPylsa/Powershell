7ba320154228edd6849a330b744547af1505ca31
PylsaPylsa <tommybeta@gmail.com>
2020-09-08 08:05:14 +0200

    Updates

---
+++ PRTG/WVD Hostpool Status/PRTG_WVDHostpool.ps1
@@ -0,0 +1,48 @@
+﻿param (
+    [string]$CredFile = "cred.txt",
+    [string]$UserName = "srv-prtg-azure@contoso.local",
+    [string]$Tenant = "Contoso",
+    [string]$HostPool = "Contoso Desktop"
+)
+$CredFile = $(Join-Path -Path $PSScriptRoot -ChildPath $CredFile )
+
+if(!(Get-module Microsoft.RDInfra.RDPowerShell)){
+    Import-Module -Name Microsoft.RDInfra.RDPowerShell | Out-Null
+}
+
+# Uncomment below to (re)create the credential object. Run this code as the same user you are 
+# saving the credentials for and on the computer you will be running this script from.
+
+#$credential = Get-Credential -UserName $UserName -Message "Enter Credentials"
+#$credential.Password | ConvertFrom-SecureString | Out-File $CredFile
+
+$PwdTxt = Get-Content $CredFile
+$SecurePwd = $PwdTxt | ConvertTo-SecureString 
+$CredObject = New-Object System.Management.Automation.PSCredential -ArgumentList $UserName, $SecurePwd
+
+$RdsAccount = Add-RdsAccount -DeploymentUrl https://rdbroker.wvd.microsoft.com -Credential $CredObject
+
+$SessionHosts = Get-RdsSessionHost -TenantName $Tenant -HostPoolName $HostPool
+$UserSessions = Get-RdsUserSession -TenantName $Tenant -HostPoolName $HostPool
+
+$ErrorStatus = 0
+$StatusMessage = ""
+
+$SessionHostsAvailable = ($SessionHosts | Where {$_.Status -eq 'Available' -and $_.AllowNewSession -eq 'True'}).Count
+$SessionTotal = $UserSessions.Count
+
+$XMLResult = @"
+<prtg>
+   <result>
+       <channel>User Sessions</channel>
+       <value>$SessionTotal</value>
+   </result>
+
+   <result>
+       <channel>Hosts available for logon</channel>
+       <value>$SessionHostsAvailable</value>
+   </result>
+</prtg>
+"@
+
+$XMLResult
--- PRTG/WVD Hostpool Status/README.md
+++ PRTG/WVD Hostpool Status/README.md
@@ -1,8 +1,6 @@
 # WVD Hosts Status PRTG Sensor
 
-This PowerShell script can be used as PRTG Advanced XML/EXE sensor and will read if it is accepting sessions from the WVD broker, whether the WVD agent is available to the WVD broker and how many users are currently logged onto the host.
-
-![Screenshot](https://github.com/PylsaPylsa/Powershell/raw/master/PRTG/WVD%20Host%20Status/Screenshot.png)
+This PowerShell script can be used as PRTG Advanced XML/EXE sensor and will read how many hosts in your hostpool are both available and accepting sessions from the WVD broker and how many users are currently logged on in the entire pool.
 
 ## Prerequisites
 
