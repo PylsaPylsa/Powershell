f5972f9b2577b807d7781fcdbb2f2c34e5325a01
PylsaPylsa <tommybeta@gmail.com>
2020-09-11 14:07:10 +0200

    Create Remove proxyAddresses with .local domain.ps1

---
+++ AD/Remove proxyAddresses with .local domain/Remove proxyAddresses with .local domain.ps1
@@ -0,0 +1,11 @@
+﻿Start-Transcript -Append -Path ~\Desktop\Transcript.log
+$Users = Get-ADUser -Filter * -SearchBase "DC=Contoso,DC=local" -Properties proxyAddresses
+
+foreach($User in $Users){
+    Write-Host "[*] Processing user $($User.UserPrincipalName)" -ForegroundColor Cyan
+    $User.proxyAddresses | ?{ $_ -like '*.local'} | %{
+        Set-ADuser -identity $User.samAccountName -remove @{proxyAddresses=$($_)}
+        Write-Host "  - Removing $($_)" 
+    }
+}
+Stop-Transcript
