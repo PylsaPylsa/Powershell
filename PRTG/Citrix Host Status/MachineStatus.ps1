dc29ebb74084ed28e2521ff51e6b989e6bb6b66d
PylsaPylsa <tommybeta@gmail.com>
2021-02-01 12:26:44 +0100

    Update MachineStatus.ps1

--- PRTG/Citrix Host Status/MachineStatus.ps1
+++ PRTG/Citrix Host Status/MachineStatus.ps1
@@ -10,9 +10,10 @@ $DeliveryGroups = Get-BrokerDesktopGroup -AdminAddress $DeliveryController
 
 foreach($DeliveryGroup in $DeliveryGroups){
 
-    $MachinesInMaintenance = $Machines | Where-Object {$_.InMaintenanceMode -eq $True -and $_.PowerState -eq "On" -and $_.DesktopGroupName -eq $DeliveryGroup.Name}
+    $MachinesInMaintenance = $Machines | Where-Object {$_.InMaintenanceMode -eq $True -and $_.PowerState -ne "Off" -and $_.DesktopGroupName -eq $DeliveryGroup.Name}
     $MachinesPoweredOff = $Machines | Where-Object {$_.PowerState -eq "Off" -and $_.DesktopGroupName -eq $DeliveryGroup.Name}
-    $MachinesUnregistered = $Machines | Where-Object {$_.RegistrationState -eq "Unregistered" -eq $True -and $_.PowerState -eq "On" -and $_.DesktopGroupName -eq $DeliveryGroup.Name}
+    $MachinesPowerUnknown = $Machines | Where-Object {$_.PowerState -eq "Unknown" -and $_.DesktopGroupName -eq $DeliveryGroup.Name}
+    $MachinesUnregistered = $Machines | Where-Object {$_.RegistrationState -eq "Unregistered" -eq $True -and $_.PowerState -ne "Unknown" -and $_.DesktopGroupName -eq $DeliveryGroup.Name}
 
 
     $retXml += "  <result>`n"
@@ -28,6 +29,11 @@ foreach($DeliveryGroup in $DeliveryGroups){
     $retXml += "  <result>`n"
     $retXml += "    <channel>[$($DeliveryGroup.Name)] Machines unregistered</channel>`n"
     $retXml += "    <value>$($MachinesUnregistered.Count)</value>`n"
+    $retXml += "  </result>`n"
+
+    $retXml += "  <result>`n"
+    $retXml += "    <channel>[$($DeliveryGroup.Name)] Machines with unknown power status</channel>`n"
+    $retXml += "    <value>$($MachinesPowerUnknown.Count)</value>`n"
     $retXml += "  </result>`n"
 
 }
